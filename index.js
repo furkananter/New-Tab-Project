5c9988861bc4d306ac2f642d20f647b3538ffc18
furky.eth <52575351+furkananter@users.noreply.github.com>
2022-04-10 13:21:02 +0300

    final

--- index.js
+++ index.js
@@ -8,25 +8,55 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
         // Use a default background image/author
         document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
 )`
-		document.getElementById("author").textContent = `By: Dodi Achmad`
+		document.getElementById("author").textContent = `By: İsimsiz Kral`
     })
+    
 
-/**
-Challenge: Get current data on a cryptocurrency from the list below
-    * bitcoin
-    * dogecoin
-    * ethereum
-    * litecoin
+fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
+.then(res => {
+    if (!res.ok) {
+        throw Error("Something went wrong")
+    }
+    return res.json()
+})
+.then(data => {
+    document.getElementById("crypto-top").innerHTML = `
+        <img src=${data.image.small} />
+        <span>${data.name}</span>
+    `
+    document.getElementById("crypto").innerHTML += `
+        <p>🎯: $${data.market_data.current_price.usd}</p>
+        <p>👆: $${data.market_data.high_24h.usd}</p>
+        <p>👇: $${data.market_data.low_24h.usd}</p>
+    `
+})
+.catch(err => console.error(err))
 
-1. Search the API docs for an endpoint that will 
-   get you the "current data for a coin"
-   (https://www.coingecko.com/api/documentations/v3#/)
 
-2. Execute a test request from the API docs and skim through 
-   the data for anything that you may find interesting to use
-   in the dashboard
-*/
-fetch("https://api.coingecko.com/api/documentations/v3/coins/dogecoin")
-    .then(res => res.json())
-    .then(data => console.log(data))
-    .catch(err => console.log(err))
+
+
+function getCurrentTime() {
+    const date = new Date()
+    document.getElementById("time").textContent = date.toLocaleTimeString("en-us", {timeStyle: "short"})
+}
+
+setInterval(getCurrentTime, 1000)
+
+navigator.geolocation.getCurrentPosition(position => {
+    fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric`)
+        .then(res => {
+            if (!res.ok) {
+                throw Error("Weather data not available")
+            }
+            return res.json()
+        })
+        .then(data => {
+            const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+            document.getElementById("weather").innerHTML = `
+                <img src=${iconUrl} />
+                <p class="weather-temp">${Math.round(data.main.temp)}º</p>
+                <p class="weather-city">${data.name}</p>
+            `
+        })
+        .catch(err => console.error(err))
+});
